45fe34d880b9736a260af6282af504642a99a31b
Neil Owen <neil.owen@philips.com>
2022-08-11 15:35:01 -0700

    Fix pay behavior

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -163,8 +163,10 @@ function emptyCart() {
   - pay will return a negative number if there is a remaining balance
   - pay will return a positive number if money should be returned to customer
 */
+let totalPaid = 0; 
 function pay(amount) {
-  let balance = amount - cartTotal();
+  totalPaid = totalPaid + amount; 
+  let balance = totalPaid - cartTotal();
   return balance; 
 }
 
