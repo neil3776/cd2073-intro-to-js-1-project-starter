a58115fc8de421e91db670fc2982381bb006499a
Neil Owen <neil.owen@philips.com>
2022-08-11 16:33:54 -0700

    change totalPaid from var to let type

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -168,7 +168,7 @@ function emptyCart() {
   - pay will return a negative number if there is a remaining balance
   - pay will return a positive number if money should be returned to customer
 */
-var totalPaid = 0; 
+let totalPaid = 0; 
 function pay(amount) {
   totalPaid = totalPaid + amount; 
   let balance = totalPaid - cartTotal();
